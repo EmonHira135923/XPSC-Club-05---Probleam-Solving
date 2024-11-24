20a769b9ba7b8d12e7ca9179c80ba51065083891
Emon Hossain Hira <emonhossainhira231@gmail.com>
2024-11-24 23:02:41 +0600

    Add files via upload

---
+++ XPSC - 05  Probleam Solving Club/Week 05/Day 29/Day 29/OR_Permutation.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+#include<string.h>
+#include<string>
+using namespace std;
+
+void solve()
+{
+    int N;
+    cin >> N;
+
+    for(int i=N;i>=1;i--)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+
+    return 0;
+}

82ce81168f2e8cdf10d6bf5549850abf7a6c65ca
Emon Hossain Hira <emonhossainhira231@gmail.com>
2024-12-12 22:23:07 +0600

    Add files via upload

---
+++ XPSC - 05  Probleam Solving Club/Week 07/Day 43/Week 07/Day 46/A_And_Then_There_Were_K.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+#include<string.h>
+#include<string>
+using namespace std;
+
+
+void solve() 
+{
+    int n;
+    cin >> n;
+
+    int msBit=__lg(n);
+    int ans=(1<<msBit);
+    ans--;
+
+    cout<<ans<<endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+
+    return 0;
+}

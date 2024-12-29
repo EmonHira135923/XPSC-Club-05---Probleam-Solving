0acc6052ca701c96c43c8cf93e6b298b53b97935
Emon Hossain Hira <emonhossainhira231@gmail.com>
2024-12-29 21:13:09 +0600

    Add files via upload

---
+++ XPSC - 05  Probleam Solving Club/Week 09/Day 57/Day 62/A_Maximum_GCD.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+#include<string.h>
+#include<string>
+using namespace std;
+
+
+void solve() 
+{
+    int N;
+    cin >> N;
+
+    cout << N/2 << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
